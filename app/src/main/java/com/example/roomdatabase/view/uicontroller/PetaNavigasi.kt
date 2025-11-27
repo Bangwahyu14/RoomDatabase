d3157c207f6d8fd96c96611c6500a0f0bfc50715
Wahyu Agung Prasetyo <wahyu.agung.ft23@mail.umy.ac.id>
2025-11-27 15:12:48 +0700

    menambahkan fun HostNavigasi

--- app/src/main/java/com/example/roomdatabase/view/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/roomdatabase/view/uicontroller/PetaNavigasi.kt
@@ -1,6 +1,8 @@
 package com.example.roomdatabase.view.uicontroller
 
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 
@@ -8,3 +10,10 @@ import androidx.navigation.compose.rememberNavController
 fun SiswaApp(navController: NavHostController = rememberNavController(), modifier: Modifier){
     HostNavigasi(navController = navController)
 }
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HostNavigasi(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+){}
