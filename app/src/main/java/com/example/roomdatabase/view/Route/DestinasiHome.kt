3abe9669b2c0a323789041e51492b51cd3bd7e93
Wahyu Agung Prasetyo <wahyu.agung.ft23@mail.umy.ac.id>
2025-11-27 15:09:28 +0700

    menambahkan code DestinasiHome.kt

---
+++ app/src/main/java/com/example/roomdatabase/view/Route/DestinasiHome.kt
@@ -0,0 +1,9 @@
+package com.example.roomdatabase.view.Route
+
+import com.example.roomdatabase.R
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+
+    override val titleRes = R.string.app_name
+}
